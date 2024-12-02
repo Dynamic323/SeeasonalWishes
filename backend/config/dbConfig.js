17ecbad83d09b55efb8285335455e874c5ad5b79
Icom1blessed <isedebless8@gmail.com>
2024-12-02 11:21:37 +0100

    m,mJ

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,15 +1,9 @@
-// For mocking MongoDB connections
-const mockgoose = require('mockgoose').mockgoose;
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    mockgoose.prepareStorage().then(() => {
-      mongoose.connect('mongodb://localhost:27017/test', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      });
-      console.log('MongoDB connected (mocked)');
-    });
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
     process.exit(1); // Exit process with failure
