5cc31f92423369f150d43381d393faa0b4e63d93
Dy-coder <otamereisrael8@gmail.com>
2024-12-29 15:26:26 +0100

    _B

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "http://localhost:3000/api";
-// const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
+// const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const api = {
   async fetchWithAuth(url, options = {}) {
